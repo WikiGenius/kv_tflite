52c9c64b0739f10d17996607960e997fd8b08b60
WikiGenius <muhammedelyamani92@gmail.com>
2023-02-10 13:58:24 +0200

    Add info

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,7 @@
+# Author: Muhammed Elyamani
+# Date: 03/02/2023
+# GitHub: https://github.com/WikiGenius
+
 from utils.draw_boxes import draw_boxes
 from utils.model import get_model
 from utils.preprocess import preprocess
